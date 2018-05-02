0404eee5c26461937ac78dc616994f06d502d4ab
GABRIEL-JOSE LABROUSSE <gablabrousse@gmail.com>
2018-05-01 20:05:30 -0400

    r code

---
+++ indeed_data.R
@@ -0,0 +1,9 @@
+indeed_data <- read.csv("raw-data-datafest2018.csv", header = T , na.strings = c("" , "NA"))
+
+indeed_data$educationRequirement <- as.character(indeed_data$educationRequirement)
+indeed_data$educationRequirement[indeed_data$educationRequirement == "None"] <- "0"
+indeed_data$educationRequirement[indeed_data$educationRequirement == "High school"] <- "1"
+indeed_data$educationRequirement[indeed_data$educationRequirement == "Higher education"] <- "2"
+
+indeed_data$educationRequirement <- as.factor(indeed_data$educationRequirement)
+
